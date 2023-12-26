9979ff7a317d37a8c0f4b83fb55ce7792485d968
khemraj-indorse <khemraj@indorse.io>
2023-12-26 11:59:41 +0530

    chore: added function templates

---
+++ src/modules/subscriptions/index.ts
@@ -0,0 +1,13 @@
+
+import { SubExecutor__factory, Initiator__factory } from "../smart-wallet/contracts";
+
+export class Subscription {
+    async attachSubExecutorToSmartWallet(){};
+    async createSubscription(){};
+    async registerSubscription(){};
+    async modifySubscription(){};
+    async revokeSubscription(){};
+    async registerWithInitiator(){};
+    async getSubscriptions(){};
+
+}
