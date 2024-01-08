6ca7f52fc4c494390471dfe0788adc82b1d25b76
Vishaldev101 <vishal@indorse.io>
2024-01-08 12:50:09 +0530

    removed: creating dapps from apis

--- src/modules/utils/api.ts
+++ src/modules/utils/api.ts
@@ -3,81 +3,9 @@ export class ApiService {
     BASE_API_URL = "https://api.bastionwallet.io";
 
 
-    async createDapp(apiKey: string, dappName: string): Promise<string> {
+    async createSubscription(apiKey: string, amount: number, validAfter: Date, validUntil: Date, initiatorAddress: String, subscriberAddress: String, paymentInterval: number, paymentLimit : number, dappId: string) : Promise<any>{
         try {
-            const payload = { dappName };
-            const headers = {
-                "x-api-key": apiKey,
-                'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-            };
-            const endpoint = `/v1/dapp/create`
-            const response = await fetch(`${this.BASE_API_URL}${endpoint}`, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers
-            });
-            const res = await response.json();
-            if(res.statusCode === "10001") throw new Error(res.message);
-            const dappData = res?.data;
-    
-            return dappData;
-        } catch (e) {
-            throw new Error(`Error while getting sponsorship, reason: ${e.response.data.message}`);
-        }
-    }
-
-    async deleteDapp(apiKey: string, dappId: string): Promise<string> {
-        try {
-            const payload = { dappId };
-            const headers = {
-                "x-api-key": apiKey,
-                'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-            };
-            const endpoint = `/v1/dapp/delete`
-            const response = await fetch(`${this.BASE_API_URL}${endpoint}`, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers
-            });
-            const res = await response.json();
-            if(res.statusCode === "10001") throw new Error(res.message);
-            const dappData = res?.data;
-    
-            return dappData;
-        } catch (e) {
-            throw new Error(`Error while getting sponsorship, reason: ${e.response.data.message}`);
-        }
-    }
-
-    async getDapp(apiKey: string, dappId: string) : Promise<any> {
-        try {
-            const query = { dappId };
-            const headers = {
-                "x-api-key": apiKey,
-                'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-            };
-            const endpoint = `/v1/dapp/get`
-            const response = await fetch(`${this.BASE_API_URL}${endpoint}?${query}`, {
-				method: "GET",
-				headers,
-			});
-
-            const res = await response.json();
-            if(res.statusCode === "10001") throw new Error(res.message);
-            const dappData = res?.data;
-    
-            return dappData;
-        } catch (e) {
-            throw new Error(`Error while getting sponsorship, reason: ${e.response.data.message}`);
-        }
-    }
-
-    async createSubscription(apiKey: string, amount: number, validAfter: Date, validUntil: Date, initiatorAddress: String, subscriberAddress: String, paymentInterval: number, paymentLimit : number) : Promise<any>{
-        try {
-            const payload = { amount, validAfter, validUntil, initiatorAddress, subscriberAddress, paymentInterval, paymentLimit };
+            const payload = { amount, validAfter, validUntil, initiatorAddress, subscriberAddress, paymentInterval, paymentLimit , dappId};
             const headers = {
                 "x-api-key": apiKey,
                 'Accept': 'application/json',
@@ -147,6 +75,6 @@ export class ApiService {
         }
     }
 
-
 }
 
+ 
