7fbcb3a822b9a63ddd570987745738281ad361cc
khemraj-indorse <khemraj@indorse.io>
2024-01-18 11:02:29 +0530

    chore: added modify, revoke tests

--- test/modules/bastionConnect/index.test.ts
+++ test/modules/bastionConnect/index.test.ts
@@ -321,8 +321,74 @@ describe("setupSmartAccount", () => {
 		expect(trxhash).toHaveLength(66);
 	}, 700000);
 
+	it("should get a subscription for given initiator", async () => {
+		let bastion = new Bastion();
+		const bastionConnect = await bastion.bastionConnect;
+		//Pass along a gasToken to use for gas
+		const { smartAccountAddress: aaAddress, exists }  = await bastionConnect.init(provider, DEFAULT_CONFIG);
 
-	it("should initiate payment for a subscription gaslessly", async () => {
+		const initiatorAddress = "0xe32D20f42fB5dA8E68A4CcdCdFfBeB337a18Af04";
+		const {attached} = await bastionConnect.initSubscriptionModuleForWallet(initiatorAddress);
+		if(!attached){
+			await bastionConnect.attachSubscriptionModuleToWallet();
+			return;
+		}
+		const subscription = await bastionConnect.getSubscription(initiatorAddress);
+		console.log("subscription:", subscription);
+
+		const timestamp = await bastionConnect.getSubscriptionLastPaidTimestamp(initiatorAddress);
+		console.log("timestamp:", timestamp);
+
+		const history = await bastionConnect.getSubscriptionPaymentHistory(initiatorAddress);
+		console.log("history:", history);
+	}, 70000);
+
+	it.skip("should modify a subscription gaslessly", async () => {
+		let bastion = new Bastion();
+		const bastionConnect = await bastion.bastionConnect;
+		//Pass along a gasToken to use for gas
+		const { smartAccountAddress: aaAddress, exists }  = await bastionConnect.init(provider, DEFAULT_CONFIG);
+		console.log("exist",exists);
+		if(!exists){
+			const receipt = await bastionConnect.createSmartAccountByDapp();
+		}
+
+		const initiatorAddress = "0xe32D20f42fB5dA8E68A4CcdCdFfBeB337a18Af04";
+		const {attached} = await bastionConnect.initSubscriptionModuleForWallet(initiatorAddress);
+		// if(!attached){
+		// 	await bastionConnect.attachSubscriptionModuleToWallet();
+		// 	return;
+		// }
+		console.log("Subscription module initalized")
+		const amount = "10";
+		const interval = 300; //seconds
+		const validUntil = 1705138050; //1710071079; //seconds
+		const erc20TokenAddress = "0x0000000000000000000000000000000000000000";//"0x8b9EAf601F4fb98f79fD62d938D15ed2a5B1EE92";
+
+
+		const trxhash = await bastionConnect.modifySubscription(amount, interval,validUntil, erc20TokenAddress);
+		console.log("Trx hash:", trxhash);
+		expect(trxhash).toHaveLength(66);
+	}, 70000);
+
+	it.skip("should revoke a subscription gaslessly", async () => {
+		let bastion = new Bastion();
+		const bastionConnect = await bastion.bastionConnect;
+		//Pass along a gasToken to use for gas
+		const { smartAccountAddress: aaAddress, exists }  = await bastionConnect.init(provider, DEFAULT_CONFIG);
+		console.log("exist",exists);
+		if(!exists){
+			const receipt = await bastionConnect.createSmartAccountByDapp();
+		}
+
+		const initiatorAddress = "0xe32D20f42fB5dA8E68A4CcdCdFfBeB337a18Af04";
+		const {attached} = await bastionConnect.initSubscriptionModuleForWallet(initiatorAddress);
+		const trxhash = await bastionConnect.revokeSubscription();
+		console.log("Trx hash:", trxhash);
+		expect(trxhash).toHaveLength(66);
+	}, 70000);
+
+	it.skip("should initiate payment for a subscription gaslessly", async () => {
 		let bastion = new Bastion();
 		const bastionConnect = await bastion.bastionConnect;
 		//Pass along a gasToken to use for gas
